196a6f246a84a9d55da3d812c5e7fdb20052a886
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-12-04 00:38:42 -0500

    [add] validacion form

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { clientesList } from '../components/clientes/clientes.json';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Cliente } from '../models/cliente';
+
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,27 +15,49 @@ export class ClienteService {
 
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
+  
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private router: Router) { }
 
   getClientes(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(`${this.URL_ENDPOINT}`);
   }
 
   crearCliente(cliente: Cliente): Observable<Cliente> {
-    return this.http.post<Cliente>(`${this.URL_ENDPOINT}`, cliente, {headers: this.httpHeaders});
+    return this.http.post<Cliente>(`${this.URL_ENDPOINT}`, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        swal.fire('Error al crear el usuario', e.error.error, 'error');
+        return throwError(e);
+      })
+    )
   }
 
   obtenerCliente(id: number): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.URL_ENDPOINT}${id}`);
+    return this.http.get<Cliente>(`${this.URL_ENDPOINT}${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes']);
+        swal.fire('Error al obtener el usuario', e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    )
   }
 
   actulizarCliente(cliente: Cliente): Observable<Cliente> {
-    return this.http.put<Cliente>(`${this.URL_ENDPOINT}${cliente.id}`, cliente, {headers: this.httpHeaders});
+    return this.http.put<Cliente>(`${this.URL_ENDPOINT}${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        swal.fire('Error al actualizar el usuario', e.error.error, 'error');
+        return throwError(e);
+      })
+    )
   }
 
   eliminarCliente(id: number): Observable<Cliente> {
-    return this.http.delete<Cliente>(`${this.URL_ENDPOINT}${id}`);
+    return this.http.delete<Cliente>(`${this.URL_ENDPOINT}${id}`).pipe(
+      catchError(e => {
+        swal.fire('Error al eliminar el usuario', e.error.error, 'error');
+        return throwError(e);
+      })
+    )
   }
 
 
